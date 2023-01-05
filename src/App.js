8caf39a6fe770272425c6e2f9bb27b3678dcfd0a
T34Virus <treindle@gmail.com>
2023-01-04 21:00:46 -0800

    Update: added pictures, config, logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+const cards = require('./config/config.json')
 
 function App() {
+
+  function getAllCards() {
+    let tempArray = [];
+    for (const card in cards) {
+      tempArray.push(
+        <div key={cards[card].name} className="cardBox flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src={cards[card].image_path} />
+            </div>
+            <div className="flip-card-back">
+              <p>{cards[card].description}</p>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return tempArray;
+  }
+
+  function showCard() {
+    const randoNumber = Math.floor(Math.random() * Object.keys(cards).length);
+
+    const cardElements = document.getElementsByClassName('flip-card');
+
+    for (const card in cards) {
+      cardElements[card].classList.remove('show')
+    }
+    
+    cardElements[randoNumber].classList.add('show');
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>ur mom</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +50,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <button id="rando" onClick={showCard}>Click me</button>
+      <div id="cardContainer">
+        {
+          getAllCards()
+        }
+      </div>
     </div>
   );
 }
